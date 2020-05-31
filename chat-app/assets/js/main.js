d3b57c891e67b28159d80c77d134357f5ce863bf
Kieran Tallinger <dellingerkieran@gmail.com>
2020-05-31 15:43:18 -0700

    Created 'database' with a server-side array and refactored current code to access it with an http request

--- chat-app/assets/js/main.js
+++ chat-app/assets/js/main.js
@@ -4,9 +4,31 @@ const messageBoard = $('#messages')
 
 function startApp() {
   $('body').on('click', '#send', sendMessage);
+  getMessages();
+}
+
+function getMessages() {
+  $.ajax({
+    url: 'http://localhost:3000/api/messages',
+    method: "GET",
+    success: handleGetMessage,
+    error: getMessageError
+  })
 }
 
 function sendMessage(message) {
-  event.preventDefault();
-  messageBoard.append(`<div class="mb-3"><h4> ${message.name} </h4> <p> ${message.text} </p></div>`);
+  if (!event) {
+    messageBoard.append(`<div class="mb-3"><h4> ${message.name} </h4> <p> ${message.text} </p></div>`);
+  } else {
+    event.preventDefault();
+    messageBoard.append(`<div class="mb-3"><h4> ${message.name} </h4> <p> ${message.text} </p></div>`);
+  }
+}
+
+function handleGetMessage(data) {
+  data.forEach(sendMessage);
+}
+
+function getMessageError(err) {
+  console.error(err);
 }
--- chat-app/server.js
+++ chat-app/server.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname))
+
+const messages = [
+  { name: "Van", text: "First" },
+  { name: "Caroline", text: "Second" }
+]
+app.get('/api/messages', (req, res, next) => {
+  res.send(messages);
+})
+
 const server = app.listen(3000, () => {
   console.log('server is listening in on port', server.address().port)
 });
