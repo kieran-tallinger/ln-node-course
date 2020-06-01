7ae04dd9ee967303bf2c4c15c21b87e72956e4d4
Kieran Tallinger <dellingerkieran@gmail.com>
2020-06-01 13:58:44 -0700

    Fixed factorial chat items growth issue by creating seperate ajax call for new messages

--- chat-app/assets/js/main.js
+++ chat-app/assets/js/main.js
@@ -11,10 +11,28 @@ function getMessages() {
   $.ajax({
     url: 'http://localhost:3000/api/messages',
     method: "GET",
-    success: handleGetMessage,
+    success: handleGetMessages,
     error: handleError
   })
 }
+
+function handleGetMessages(data) {
+  data.forEach(placeMessage);
+}
+
+function getNewMessage() {
+  $.ajax({
+    url: 'http://localhost:3000/api/messages',
+    method: "GET",
+    success: handleNewMessage,
+    error: handleError
+  })
+}
+
+function handleNewMessage(data) {
+  placeMessage(data[data.length - 1]);
+}
+
 
 function postMessage(name, text) {
   $.ajax({
@@ -29,6 +47,10 @@ function postMessage(name, text) {
   })
 }
 
+function handlePostMessage(data) {
+  getNewMessage();
+}
+
 function placeMessage(message) {
   if (!event) {
     messageBoard.append(`<div class="mb-3"><h4> ${message.name} </h4> <p> ${message.text} </p></div>`);
@@ -40,20 +62,13 @@ function placeMessage(message) {
 
 function handleSubmit(event) {
   event.preventDefault();
-  console.log(event)
-  let newMessage = new FormData(event.currentTarget);
-  const newName = newMessage.get('name');
-  const newText = newMessage.get('text');
-  postMessage(newName, newText);
-  event.currentTarget.reset();
-}
-
-function handleGetMessage(data) {
-  data.forEach(placeMessage);
-}
-
-function handlePostMessage(data) {
-  placeMessage(data);
+  if (event.target.id === 'send') {
+    let newMessage = new FormData(event.currentTarget);
+    const newName = newMessage.get('name');
+    const newText = newMessage.get('text');
+    postMessage(newName, newText);
+    event.currentTarget.reset();
+  }
 }
 
 function handleError(err) {
